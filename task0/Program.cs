31344dda5bd4ce27ba50331df3df45efc17eefa7
AnnaA2022 <103459299+AnnaA2022@users.noreply.github.com>
2022-04-23 23:49:58 +0300

    task0

---
+++ task0/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+if (a<b)
+{
+    Console.WriteLine($"наименьшее число {a }");
+    Console.WriteLine($"наибольшее число {b }");
+}
+else 
+{
+    Console.WriteLine($"наименьшее число {b }");
+    Console.WriteLine($"наибольшее число {a }");
+}
