35a27291ceeb600472bc9122fb1f2b59ddd84019
AnnaA2022 <103459299+AnnaA2022@users.noreply.github.com>
2022-04-23 23:57:24 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+using System;
+
+int N =int.Parse(Console.ReadLine());
+int b = 2;
+
+while (b<=N)
+{
+   if (b % 2 == 0) 
+   {
+       Console.Write($"{b} ");
+       b++;
+   }
+   else
+   {
+       b++;
+   }
+}
