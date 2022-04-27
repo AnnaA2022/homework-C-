c9a4c5617ccaa6fcb3340a40d178f277283b0530
AnnaA2022 <103459299+AnnaA2022@users.noreply.github.com>
2022-04-28 01:10:35 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*using System;
+Console.Clear();
+
+int num=new Random().Next(100,1000);
+string numS=num.ToString();
+int a = Convert.ToInt32(numS[1].ToString());
+
+Console.WriteLine($"{num}->{a}");*/
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+/*using System;
+Console.Clear();
+Console.Write("Введите число: ");
+int num=int.Parse(Console.ReadLine());
+if (num/100 == 0)
+{
+    Console.WriteLine($"{num}->третьего числа нет");
+}
+else
+{
+    string numS=num.ToString();
+    int a = Convert.ToInt32(numS[2].ToString());
+    Console.WriteLine($"{num}->{a}");
+}*/
+
+//Задача 15: Напишите программу,
+// которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+using System;
+Console.Clear();
+Console.Write("Введите порядковый номер дня недели: ");
+int num=int.Parse(Console.ReadLine());
+
+string answer = (num==6)? $"{num} день недели - выходной":(num==7)? $"{num} день недели - выходной": $" {num} день недели - не выходной";
+Console.WriteLine(answer);
